52a121611dd04a89a275c440a6c767f804227dfe
Samuel Alvarez Garcia <samuel.alvarezgarcia@student.uliege.be>
2019-04-23 14:34:36 +0200

    todolist

--- src/SoccerBall.java
+++ src/SoccerBall.java
@@ -4,3 +4,24 @@ public class SoccerBall {
 
     }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+//TODO
+//Creation effectué -> Set 1ere pieces
+/* -> Check nb element dans Data.CONNECTIONS pour déterminer si Penta ou Hexa
+-> Verifier piece par piece si 2 poly colle entre eux (convex -> concav)
+-> Placer la piece ajouté dans un tablea de "Back up" (-> piece placée)
+-> Delete de la liste le polygone ajouté (Si erreur, le rajouté à la fin et le del du tableau back up)
+
+-> END
