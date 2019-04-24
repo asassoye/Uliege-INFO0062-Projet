a7f18c5c909f3f7fa3e960c3cb8ce78fd1773568
Andrew Sassoye <andrew@sassoye.be>
2019-04-24 15:30:11 +0200

    Chipolata

--- src/Polygon.java
+++ src/Polygon.java
@@ -1,28 +1,63 @@
 abstract class Polygon {
-    private int[] concavity;
+    private int position;
     private int rotation;
     private int type;
+    private int[] concavity;
+    private int[] connections = null;
 
     Polygon(int type, int[] concavity) {
         this.concavity = concavity;
         this.type = type;
     }
 
-    public void rotate() {
-        if(this.isRotable()){
-            int tmp = this.concavity[0];
-            int j = 1;
+    public boolean rotate() {
+        int tmp = this.concavity[0];
+        int j = 1;
 
-            while(j < this.concavity.length){
-                this.concavity[j-1] = this.concavity[j];
-                j++;
-            }
-            this.concavity[j] = tmp;
-            this.rotation++;
+        while (j < this.concavity.length) {
+            this.concavity[j - 1] = this.concavity[j];
+            j++;
         }
+        this.concavity[j - 1] = tmp;
+
+        if (this.isRotable()) {
+            this.rotation++;
+            return true;
+        } else {
+            this.rotation = 0;
+            return false;
+        }
+
     }
 
-    public boolean isRotable() {
-        return this.rotation < this.concavity.length;
+
+    private boolean isRotable() {
+        System.out.println(this.concavity.length);
+
+        return this.rotation < this.concavity.length - 1;
+    }
+
+    int getType() {
+        return this.type;
+    }
+
+    void printPiece() {
+        System.out.println("Position " + this.position + " - Element " + this.type + " - Orientation " + this.rotation);
+    }
+
+    int[] getConnections() {
+        return connections;
+    }
+
+    void setConnections(int[] connections) {
+        this.connections = connections;
+    }
+
+    public int getPosition() {
+        return position;
+    }
+
+    void setPosition(int position) {
+        this.position = position;
     }
 }
--- src/SoccerBall.java
+++ src/SoccerBall.java
@@ -1,15 +1,78 @@
 import java.util.LinkedList;
 
 public class SoccerBall {
+    private LinkedList<Polygon> availablePieces;
+    private LinkedList<Polygon> orderedPieces;
+
+
+    private SoccerBall() {
+        this.availablePieces = PolygonFactory.createPieces();
+        this.orderedPieces = new LinkedList<>();
+    }
 
     public static void main(String[] args) {
-        LinkedList<Polygon> orderedPieces = new LinkedList<>();
-        LinkedList<Polygon> availablePieces = PolygonFactory.createPieces();
+        SoccerBall soccerBall = new SoccerBall();
+        soccerBall.solve();
+        soccerBall.printOrderedPieces();
+    }
 
-        assert availablePieces.size() == Data.CONNECTIONS.length : "Nombre de pieces incorrect";
+    private void solve() {
+        this.orderedPieces.add(this.availablePieces.get(20));
 
-        System.out.println("Start");
+        this.orderedPieces.getFirst().setConnections(Data.CONNECTIONS[0]);
+        this.orderedPieces.getFirst().setPosition(1);
 
+        this.orderedPieces.add(this.availablePieces.get(1));
+
+        this.orderedPieces.get(1).setConnections(Data.CONNECTIONS[1]);
+        this.orderedPieces.get(1).setPosition(2);
+
+        int[] connections = Data.CONNECTIONS[3];
+        int lenght = connections.length;
+
+        Polygon piece = this.getNextType(0, lenght);
+        LinkedList<Polygon> connectedPieces = this.getConnectedPieces(piece, connections);
+
+        System.out.println("Coucou");
+
+
+    }
+
+    private LinkedList<Polygon> getConnectedPieces(Polygon piece, int[] connections) {
+        LinkedList<Polygon> connectedPieces = new LinkedList<>();
+
+        for (Polygon orderedPiece : this.orderedPieces) {
+            for (int connection : connections) {
+                for (int k = 0; k < orderedPiece.getConnections().length; ++k) {
+                    if (orderedPiece.getConnections()[k] == connection) {
+                        connectedPieces.add(orderedPiece);
+                    }
+                }
+            }
+        }
+
+        return connectedPieces;
+    }
+
+
+    private Polygon getNextType(int actual, int sides) {
+        for (Polygon piece : this.availablePieces) {
+            if (piece.getType() > actual) {
+                if (sides == 5 && piece instanceof Pentagon) {
+                    return piece;
+                } else if (sides == 6 && piece instanceof Hexagon) {
+                    return piece;
+                }
+            }
+        }
+
+        return null;
+    }
+
+    private void printOrderedPieces() {
+        for (Polygon orderedPiece : orderedPieces) {
+            orderedPiece.printPiece();
+        }
     }
 }
 
